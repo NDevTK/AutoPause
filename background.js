424898acd1729c6f5c69eb92d139a08a6ba92e17
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-09-21 12:16:44 +0100

    Comments

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
 activeTab = false;
-sounds = [];
+sounds = []; // List of tab ids that have had audio
 
 chrome.tabs.onActivated.addListener(activeInfo => {
-  activeTab = activeInfo.tabId;
-  chrome.tabs.sendMessage(activeTab, false);
+  activeTab = activeInfo.tabId; // Current tab
+  chrome.tabs.sendMessage(activeTab, false); // Resume when active
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo)  => {
-  if(!changeInfo.hasOwnProperty("audible")) return
+  if(!changeInfo.hasOwnProperty("audible")) return // Bool that contains if audio is playing on tab
   if(changeInfo.audible) {
-    if(!sounds.includes(changeInfo)) sounds.push(tabId);
+    if(!sounds.includes(changeInfo)) sounds.push(tabId); // Tab has sound
   }
-  if(tabId === activeTab) Broardcast(changeInfo.audible, activeTab);
+  if(tabId === activeTab) Broardcast(changeInfo.audible, activeTab); // Tell the other tabs the state of the active tab
 });
 
 async function Broardcast(message, exclude = false) {
-  sounds.forEach(id => {
+  sounds.forEach(id => { // Only for tabs that have had sound
       if(id === exclude) return
       chrome.tabs.sendMessage(id, message);
   });

6679e2f050c9c700ac76aa06aff1af61baa54614
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-09 00:31:11 +0000

    Update ContentScript.js

--- ContentScript.js
+++ ContentScript.js
@@ -1,34 +1,32 @@
 "use strict";
 var ActiveAudio = false;
+var Elements = [];
 
 chrome.runtime.onMessage.addListener(async (state) => {
     ActiveAudio = state; // React based on state of active tab
-    main();
+    (ActiveAudio) ? pause(): resume();
 });
 
-document.onplay = main;
+window.addEventListener('play', function(event) {
+    let src = event.srcElement;
+    if (src instanceof HTMLMediaElement === true) {
+        if (ActiveAudio) src.pause();
+        if (!Elements.includes(src)) Elements.push(src);
+    }
+}, true);
 
-async function main() {
-    (ActiveAudio) ? pause(): resume();
-}
-
-function checkInvalid(e) {
-    return (e.getRootNode() instanceof ShadowRoot === true || e instanceof HTMLMediaElement === false);
-}
 
 async function pause() {
-    let Elements = [...document.getElementsByTagName("*")];
     Elements.forEach(e => {
-        if (checkInvalid(e) || e.paused) return;
+        if (e.paused) return;
         e.pause();
         e.wasPlaying = true;
     });
 }
 
 async function resume() {
-    let Elements = [...document.getElementsByTagName("*")];
     Elements.forEach(e => {
-        if (checkInvalid(e) || !e.wasPlaying) return
+        if (!e.wasPlaying) return
         e.play();
         e.wasPlaying = false;
     });

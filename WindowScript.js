2157a54b1a4ae0f0404676938a8a901754b4cdbd
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-10 15:47:28 +0000

    Update WindowScript.js

--- WindowScript.js
+++ WindowScript.js
@@ -2,20 +2,20 @@
 // Automaticly add media elements to DOM.
 (function() {
     "use strict";
-	var made = false;
+    var made = false;
     const play = HTMLMediaElement.prototype.play;
-	
+
     HTMLMediaElement.prototype.play = function() {
         let result = play.apply(this, arguments);
-		if(!document.contains(this)) {
-			if(made === false) {
-				const div = document.createElement('div');
-				div.hidden = true;
-				document.head.appendChild(div);
-				made = true;
-			}
-			div.appendChild(this);
-		}
+        if (!document.contains(this)) {
+            if (made === false) {
+                const div = document.createElement('div');
+                div.hidden = true;
+                document.head.appendChild(div);
+                made = true;
+            }
+            div.appendChild(this);
+        }
         return result
     }
 })();

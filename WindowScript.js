104cf0ddfa98304ee887205da7b66b4d3966d73c
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-10 15:47:13 +0000

    Only create div if used

--- WindowScript.js
+++ WindowScript.js
@@ -2,17 +2,20 @@
 // Automaticly add media elements to DOM.
 (function() {
     "use strict";
+	var made = false;
     const play = HTMLMediaElement.prototype.play;
-
-    const div = document.createElement('div');
-    div.hidden = true;
-    document.head.appendChild(div);
-
+	
     HTMLMediaElement.prototype.play = function() {
         let result = play.apply(this, arguments);
-        if (!document.contains(this)) {
-            div.appendChild(this);
-        }
+		if(!document.contains(this)) {
+			if(made === false) {
+				const div = document.createElement('div');
+				div.hidden = true;
+				document.head.appendChild(div);
+				made = true;
+			}
+			div.appendChild(this);
+		}
         return result
     }
 })();

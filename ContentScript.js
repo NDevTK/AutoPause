9bbed9f5037af85188f333b875160c809331f906
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-11 18:00:18 +0000

    Auto resume fixes

--- ContentScript.js
+++ ContentScript.js
@@ -23,30 +23,47 @@ function injectScript(file_path) {
 window.addEventListener('play', function(event) {
     let src = event.srcElement;
     if (src instanceof HTMLMediaElement === true) {
-        if (ActiveAudio) src.pause();
+        if (ActiveAudio) pauseElement(src);
         if (!Elements.includes(src)) Elements.push(src);
     }
 }, true);
 
+// Dont tell the media please
+window.addEventListener('ratechange', function(event) {
+    let src = event.srcElement;
+    if (src instanceof HTMLMediaElement === true) {
+        if (ActiveAudio && src.playbackRate === 0) {
+            event.stopPropagation();
+        } else if (ActiveAudio) {
+            src.pause();
+        }
+    }
+}, true);
+
+function pauseElement(e) {
+    if (ActiveAudio === null) {
+        e.pause();
+    } else {
+        e.wasVolume = e.volume;
+        e.wasPlaybackRate = e.playbackRate;
+        e.volume = 0;
+        e.playbackRate = 0;
+        e.wasPlaying = true;
+    }
+}
 
 async function pause() {
     Elements.forEach(e => {
         if (e.paused || e.playbackRate === 0) return;
-        e.wasVolume = e.volume;
-        e.wasPlaybackRate = e.playbackRate;
-        e.volume = 0
-        e.playbackRate = 0
-        e.wasPlaying = true;
+        pauseElement(e);
     });
 }
 
 async function resume() {
     Elements.forEach(e => {
         if (!e.wasPlaying) return
+        if (e.paused) e.play();
         e.volume = e.wasVolume;
-        if (ActiveAudio === null) {
-            e.pause();
-        }
         e.playbackRate = e.wasPlaybackRate;
         e.wasPlaying = false;
     });

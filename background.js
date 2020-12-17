4a4e94275ad351af07a3fe901fecaed40d6866d6
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-17 00:31:43 +0000

    bug fixes

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 "use strict";
-var activeTab = null;
 var sounds = []; // List of tab ids that have had audio
 var options = {};
 
@@ -9,7 +8,13 @@ chrome.storage.sync.get("options", function(result) {
 
 chrome.windows.onFocusChanged.addListener(id => {
     if (id === -1) return
-    checkOrigin();
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, tab => {
+        if (tab.length !== 1) return
+        checkOrigin(tab[0]);
+    });
 });
 
 chrome.commands.onCommand.addListener(command => {
@@ -32,41 +37,41 @@ chrome.commands.onCommand.addListener(command => {
     }
 });
 
-function checkOrigin() {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, tab => {
-        if (tab.length !== 1 || tab[0].active === false || tab[0].id === undefined || tab[0].url === undefined) return
-        activeTab = tab[0].id;
-        let message = tab[0].audible;
-        if (options.hasOwnProperty("disableresume")) {
-            chrome.tabs.sendMessage(activeTab, null, sendHandler); // Only allow playback
-            if (message === false) return
-        } else {
-            chrome.tabs.sendMessage(activeTab, false, sendHandler); // Resume when active
-        }
-        Broadcast(message, activeTab);
-    });
+function checkOrigin(tab) {
+    if (tab.active === false || tab.id === undefined || tab.url === undefined) return
+    let message = tab.audible;
+    if (options.hasOwnProperty("disableresume")) {
+        chrome.tabs.sendMessage(tab.id, null, sendHandler); // Only allow playback
+        if (message === false) return
+    } else {
+        chrome.tabs.sendMessage(tab.id, false, sendHandler); // Resume when active
+    }
+    Broadcast(message, tab.id);
 }
 
 function sendHandler() {
     let lastError = chrome.runtime.lastError;
 }
 
-chrome.tabs.onActivated.addListener(checkOrigin);
-
-chrome.tabs.onRemoved.addListener(tabId => {
-    sounds.splice(sounds.indexOf(tabId), 1);
+chrome.tabs.onActivated.addListener(info => {
+    chrome.tabs.get(info.tabId, tab => {
+        checkOrigin(tab);
+    });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+chrome.tabs.onRemoved.addListener(tabId => {
+    sounds = sounds.filter(id => id !== tabId);
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (!changeInfo.hasOwnProperty("audible")) return // Bool that contains if audio is playing on tab
     if (changeInfo.audible && !sounds.includes(tabId)) {
         sounds.push(tabId);
     }
     if (options.hasOwnProperty("disableresume") && changeInfo.audible === false) return
-    if (tabId === activeTab) Broadcast(changeInfo.audible, activeTab); // Tell the other tabs the state of the active tab
+    if (tab.active) {
+        Broadcast(changeInfo.audible, tabId); // Tell the other tabs the state of the active tab
+    }
 });
 
 async function Broadcast(message, exclude = false) {

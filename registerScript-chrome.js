c11a483000b73bbbde0214f50647dcf28fe2c64a
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-24 13:10:32 +0000

    Update registerScript-chrome.js

--- registerScript-chrome.js
+++ registerScript-chrome.js
@@ -12,7 +12,4 @@ chrome.runtime.onInstalled.addListener(function(details) {
             })]
         }]);
     });
-    if (details.reason == "install") {
-        chrome.runtime.openOptionsPage();
-    }
 });

8ef26af89745734d3ebd0b2ed1e80d059d2d8128
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-24 13:06:28 +0000

    Create registerScript-firefox.js

---
+++ registerScript-firefox.js
@@ -0,0 +1,26 @@
+"use strict";
+var contentscript = null;
+
+function registerScriptFirefox() {
+    if (contentscript !== null) {
+        contentscript.unregister();
+        contentscript = null;
+    }
+    browser.permissions.getAll(async p => {
+        if (p.origins.length < 1) return
+        contentscript = await browser.contentScripts.register({
+            "js": [{
+                file: "ContentScript.js"
+            }],
+            "matches": p.origins,
+            "allFrames": true,
+            "runAt": "document_start"
+        });
+    });
+}
+
+if (typeof(browser) !== "undefined") {
+    browser.permissions.onAdded.addListener(registerScriptFirefox);
+    browser.permissions.onRemoved.addListener(registerScriptFirefox);
+    registerScriptFirefox();
+}

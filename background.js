09774e84b69db106b4e152d87c095fa04d7f4467
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-24 13:17:28 +0000

    Remove browser dependent code

--- background.js
+++ background.js
@@ -6,48 +6,7 @@ chrome.storage.sync.get("options", function(result) {
     if (typeof result["options"] === 'object' && result["options"] !== null) options = result["options"];
 });
 
-var contentscript = null;
-
-function registerScriptFirefox() {
-    if (contentscript !== null) {
-        contentscript.unregister();
-        contentscript = null;
-    }
-    browser.permissions.getAll(async p => {
-        if (p.origins.length < 1) return
-        contentscript = await browser.contentScripts.register({
-            "js": [{
-                file: "ContentScript.js"
-            }],
-            "matches": p.origins,
-            "allFrames": true,
-            "runAt": "document_start"
-        });
-    });
-}
-
-if (typeof(browser) !== "undefined") {
-    browser.permissions.onAdded.addListener(registerScriptFirefox);
-    browser.permissions.onRemoved.addListener(registerScriptFirefox);
-    registerScriptFirefox();
-}
-
 chrome.runtime.onInstalled.addListener(function(details) {
-    if (typeof(browser) === "undefined") {
-        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-            chrome.declarativeContent.onPageChanged.addRules([{
-                conditions: [new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {
-                        schemes: ["http", "https", "file", "ftp"]
-                    }
-                })],
-                actions: [new chrome.declarativeContent.RequestContentScript({
-                    allFrames: true,
-                    js: ["ContentScript.js"]
-                })]
-            }]);
-        });
-    }
     if (details.reason == "install") {
         chrome.runtime.openOptionsPage();
     }

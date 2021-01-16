0568e7c5d6c847ac0ed3dee5f4aa92b4838f7d5d
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-16 20:38:59 +0000

    Update options-firefox.js

--- options-firefox.js
+++ options-firefox.js
@@ -1,3 +1,5 @@
+'use strict';
+/* global browser */
 var commands = [];
 var shortcuts = document.getElementById('shortcuts');
 
@@ -6,7 +8,10 @@ function commandsUpdate() {
     const id = command.name + "shortcut";
     let element = document.getElementById(id);
     if (element.value !== command.shortcut) {
-    
+      browser.commands.update({
+        name: command.name,
+        shortcut: command.shortcut
+      });
     }
   });
 }
@@ -21,26 +26,24 @@ window.addEventListener('keyup', event => {
 getCommands();
 
 function getCommands() {
-  chrome.commands.getAll(c => {
-    commands = c;
-    commands.forEach(command => {
-      const id = command.name + "shortcut";
-      let element = document.getElementById(id);
-      if (element) {
-        element.value = command.shortcut;
-        return
-      }
-      let label = document.createElement("label");
-      label.innerText = command.description + " ";
-      label.setAttribute("for", id);
-      let input = document.createElement("input");
-      input.type = "text";
-      input.id = id;
-      input.value = command.shortcut;
-      label.appendChild(input);
-      shortcuts.appendChild(label);
-      let br = document.createElement("br");
-      shortcuts.appendChild(br);
-    });
+  commands = await browser.commands.getAll();
+  commands.forEach(command => {
+    const id = command.name + "shortcut";
+    let element = document.getElementById(id);
+    if (element) {
+      element.value = command.shortcut;
+      return
+    }
+    let label = document.createElement("label");
+    label.innerText = command.description + " ";
+    label.setAttribute("for", id);
+    let input = document.createElement("input");
+    input.type = "text";
+    input.id = id;
+    input.value = command.shortcut;
+    label.appendChild(input);
+    shortcuts.appendChild(label);
+    let br = document.createElement("br");
+    shortcuts.appendChild(br);
   });
-}
+};

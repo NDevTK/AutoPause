6bc0fe400f3f3cca729fb958ae678fcd21098e71
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-16 02:53:17 +0000

    formating

--- registerScript-firefox.js
+++ registerScript-firefox.js
@@ -4,21 +4,21 @@
 var contentscript = null;
 
 function registerScriptFirefox() {
-    if (contentscript !== null) {
-        contentscript.unregister();
-        contentscript = null;
-    }
-    browser.permissions.getAll(async p => {
-        if (p.origins.length < 1) return
-        contentscript = await browser.contentScripts.register({
-            "js": [{
-                file: "ContentScript.js"
-            }],
-            "matches": p.origins,
-            "allFrames": true,
-            "runAt": "document_start"
-        });
+  if (contentscript !== null) {
+    contentscript.unregister();
+    contentscript = null;
+  }
+  browser.permissions.getAll(async p => {
+    if (p.origins.length < 1) return
+    contentscript = await browser.contentScripts.register({
+      "js": [{
+        file: "ContentScript.js"
+      }],
+      "matches": p.origins,
+      "allFrames": true,
+      "runAt": "document_start"
     });
+  });
 }
 
 browser.permissions.onAdded.addListener(registerScriptFirefox);

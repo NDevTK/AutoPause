b5539f9c94a4672e918c224b2304b8a93d270fc9
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-16 02:50:47 +0000

    formating

--- ContentScript.js
+++ ContentScript.js
@@ -3,146 +3,164 @@
 
 // Script should only run once
 if (tabPause === undefined) {
-    var tabPause = false;
-    var Elements = new Set();
+  var tabPause = false;
+  var Elements = new Set();
 }
 
 chrome.runtime.onMessage.addListener(message => {
-    switch (message) {
-        case "toggleFastPlayback":
-            toggleRate();
-            break
-        case "pause":
-            pause();
-            break
-        case "play":
-            resume(true);
-            break
-        case "allowplayback":
-            resume(false);
-            break
-    }
-    
+  switch (message) {
+    case "toggleFastPlayback":
+      toggleRate();
+      break
+    case "pause":
+      pause();
+      break
+    case "play":
+      resume(true);
+      break
+    case "allowplayback":
+      resume(false);
+      break
+  }
+
 });
 
 window.addEventListener('beforeunload', () => {
-    Elements.clear();
-    chrome.runtime.sendMessage("pause");
-}, {passive: true});
+  Elements.clear();
+  chrome.runtime.sendMessage("pause");
+}, {
+  passive: true
+});
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Adds content to DOM needed because of isolation
-    injectScript("WindowScript.js");
-}, {passive: true});
+  // Adds content to DOM needed because of isolation
+  injectScript("WindowScript.js");
+}, {
+  passive: true
+});
 
 // Controlled by global fast forward shortcut
 function toggleRate() {
-    Elements.forEach(e => {
-        if (e.paused || e.playbackRate === 0 || e.wasPlaying) return;
-        if (e.wasPlaybackRate && e.playbackRate > 1) {
-            e.playbackRate = e.wasPlaybackRate;
-        } else {
-            e.wasPlaybackRate = e.playbackRate;
-            e.playbackRate = 2;
-        }
-    });
+  Elements.forEach(e => {
+    if (e.paused || e.playbackRate === 0 || e.wasPlaying) return;
+    if (e.wasPlaybackRate && e.playbackRate > 1) {
+      e.playbackRate = e.wasPlaybackRate;
+    } else {
+      e.wasPlaybackRate = e.playbackRate;
+      e.playbackRate = 2;
+    }
+  });
 }
 
 function injectScript(file_path) {
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('crossorigin', 'anonymous');
-    script.setAttribute('src', chrome.runtime.getURL(file_path));
-    document.head.appendChild(script);
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('crossorigin', 'anonymous');
+  script.setAttribute('src', chrome.runtime.getURL(file_path));
+  document.head.appendChild(script);
 }
 
 // On media play event
 window.addEventListener('play', function(event) {
-    const src = event.srcElement;
-    if (src instanceof HTMLMediaElement) {
-        if (src.muted === false) {
-            chrome.runtime.sendMessage("play");
-        } else {
-            chrome.runtime.sendMessage("playMuted");
-        }
-        src.wasMuted = src.muted;
-        if (tabPause) pauseElement(src);
-        if (!Elements.has(src)) {
-            Elements.add(src);
-        }
+  const src = event.srcElement;
+  if (src instanceof HTMLMediaElement) {
+    if (src.muted === false) {
+      chrome.runtime.sendMessage("play");
+    } else {
+      chrome.runtime.sendMessage("playMuted");
     }
-}, {capture: true, passive: true});
+    src.wasMuted = src.muted;
+    if (tabPause) pauseElement(src);
+    if (!Elements.has(src)) {
+      Elements.add(src);
+    }
+  }
+}, {
+  capture: true,
+  passive: true
+});
 
 window.addEventListener('volumechange', function(event) {
-    const src = event.srcElement;
-    if (src instanceof HTMLMediaElement) {
-        if (src.wasMuted !== src.muted && !src.paused) {
-            if(src.muted) {
-                chrome.runtime.sendMessage("playMuted");
-            } else {
-                chrome.runtime.sendMessage("play");
-            }
-        }
-        src.wasMuted = src.muted;
+  const src = event.srcElement;
+  if (src instanceof HTMLMediaElement) {
+    if (src.wasMuted !== src.muted && !src.paused) {
+      if (src.muted) {
+        chrome.runtime.sendMessage("playMuted");
+      } else {
+        chrome.runtime.sendMessage("play");
+      }
     }
-}, {capture: true, passive: true});
+    src.wasMuted = src.muted;
+  }
+}, {
+  capture: true,
+  passive: true
+});
 
 window.addEventListener("pause", event => {
-    setTimeout(() => {
-        onPause(event);	
-    }, 100);
-}, {capture: true, passive: true});
+  setTimeout(() => {
+    onPause(event);
+  }, 100);
+}, {
+  capture: true,
+  passive: true
+});
 
 window.addEventListener("abort", event => {
-    onPause(event);
-}, {capture: true, passive: true});
+  onPause(event);
+}, {
+  capture: true,
+  passive: true
+});
 
 function onPause(event) {
-    const src = event.srcElement;
-    if (src instanceof HTMLMediaElement && src.paused) {
-        Elements.delete(src);
-        const audibleElements = [...Elements].filter(e => !e.muted);
-        if (audibleElements.length === 0) chrome.runtime.sendMessage("pause");
-    }
+  const src = event.srcElement;
+  if (src instanceof HTMLMediaElement && src.paused) {
+    Elements.delete(src);
+    const audibleElements = [...Elements].filter(e => !e.muted);
+    if (audibleElements.length === 0) chrome.runtime.sendMessage("pause");
+  }
 }
 
 // Dont tell the media please
 window.addEventListener('ratechange', function(event) {
-    const src = event.srcElement;
-    if (src instanceof HTMLMediaElement === true) {
-        if (tabPause && src.playbackRate === 0) {
-            event.stopPropagation();
-        }
+  const src = event.srcElement;
+  if (src instanceof HTMLMediaElement === true) {
+    if (tabPause && src.playbackRate === 0) {
+      event.stopPropagation();
     }
-}, {capture: true});
+  }
+}, {
+  capture: true
+});
 
 function pauseElement(e) {
-    // If media attempts to play when it should be paused dont change its old values.
-    if (!e.wasPlaying) {
-        e.wasVolume = e.volume;
-        e.wasPlaybackRate = e.playbackRate;
-    }
-    e.volume = 0;
-    e.playbackRate = 0;
-    e.wasPlaying = true;
+  // If media attempts to play when it should be paused dont change its old values.
+  if (!e.wasPlaying) {
+    e.wasVolume = e.volume;
+    e.wasPlaybackRate = e.playbackRate;
+  }
+  e.volume = 0;
+  e.playbackRate = 0;
+  e.wasPlaying = true;
 }
 
 async function pause() {
-    tabPause = true;
-    Elements.forEach(e => {
-        if (e.paused || e.playbackRate === 0) return;
-        pauseElement(e);
-    });
+  tabPause = true;
+  Elements.forEach(e => {
+    if (e.paused || e.playbackRate === 0) return;
+    pauseElement(e);
+  });
 }
 
 async function resume(shouldPlay) {
-    tabPause = false;
-    Elements.forEach(e => {
-        if (!e.wasPlaying) return
-        // Pause foreground media normaly
-        if (shouldPlay === false) e.pause();
-        e.volume = e.wasVolume;
-        e.playbackRate = e.wasPlaybackRate;
-        e.wasPlaying = false;
-    });
+  tabPause = false;
+  Elements.forEach(e => {
+    if (!e.wasPlaying) return
+    // Pause foreground media normaly
+    if (shouldPlay === false) e.pause();
+    e.volume = e.wasVolume;
+    e.playbackRate = e.wasPlaybackRate;
+    e.wasPlaying = false;
+  });
 }

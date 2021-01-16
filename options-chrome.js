e6d9f25fae84b478556dfa884138ec87482e4232
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-16 21:30:50 +0000

    Update options-chrome.js

--- options-chrome.js
+++ options-chrome.js
@@ -2,11 +2,11 @@
 /* global chrome */
 
 var shortcuts = document.getElementById('shortcuts');
-var btn = document.createElement("button");
-btn.innerText = "Edit shortcuts";
+var btn = document.createElement('button');
+btn.innerText = 'Edit shortcuts';
 
-btn.addEventListener("click", () => {
-  chrome.tabs.create({ url: "chrome://extensions/shortcuts" });
+btn.addEventListener('click', () => {
+  chrome.tabs.create({ url: 'chrome://extensions/shortcuts' });
 });
 
 shortcuts.appendChild(btn);

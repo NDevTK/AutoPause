e9b95508d17506271e94d92b4997de99ba2980f4
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-16 03:12:11 +0000

    Update registerScript-chrome.js

--- registerScript-chrome.js
+++ registerScript-chrome.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 /*global chrome*/
 chrome.runtime.onInstalled.addListener(() => {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {
-          schemes: ["http", "https", "file", "ftp"]
+          schemes: ['http', 'https', 'file', 'ftp']
         }
       })],
       actions: [new chrome.declarativeContent.RequestContentScript({
         allFrames: true,
-        js: ["ContentScript.js"]
+        js: ['ContentScript.js']
       })]
     }]);
   });

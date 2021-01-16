b34fc153aaff4abeea7996913aaba97cc1f6c98e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-16 02:52:05 +0000

    Formating

--- WindowScript.js
+++ WindowScript.js
@@ -1,25 +1,25 @@
 // Code here is exposed to the website.
 // Automaticly add media elements to DOM.
 (function() {
-    "use strict";
-    const play = window.HTMLMediaElement.prototype.play;
-    let div = null;
-    window.HTMLMediaElement.prototype.play = function() {
-        if (this instanceof HTMLMediaElement && !document.contains(this)) {
-            if (!document.contains(div)) {
-                div = document.createElement('div');
-                div.hidden = true;
-                document.head.appendChild(div);
-                // If media gets paused remove it from the div
-                div.addEventListener("pause", event => {
-                    const src = event.srcElement;
-                    if (src instanceof HTMLMediaElement) {
-                        div.removeChild(src);
-                    }
-                }, {passive: true, capture: true});
-            }
-            div.appendChild(this);
-        }
-        return play.apply(this, arguments);
+  "use strict";
+  const play = window.HTMLMediaElement.prototype.play;
+  let div = null;
+  window.HTMLMediaElement.prototype.play = function() {
+    if (this instanceof HTMLMediaElement && !document.contains(this)) {
+      if (!document.contains(div)) {
+        div = document.createElement('div');
+        div.hidden = true;
+        document.head.appendChild(div);
+        // If media gets paused remove it from the div
+        div.addEventListener("pause", event => {
+          const src = event.srcElement;
+          if (src instanceof HTMLMediaElement) {
+            div.removeChild(src);
+          }
+        }, {passive: true, capture: true});
+      }
+      div.appendChild(this);
     }
+    return play.apply(this, arguments);
+  }
 })();

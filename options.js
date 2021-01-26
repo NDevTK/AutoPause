95d2822f327ec83bfcd8eecdb3fb0603a693d273
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-01-26 13:10:40 +0000

    add ignoretabchange

--- options.js
+++ options.js
@@ -4,7 +4,7 @@ var permissions = [];
 var options = {};
 
 // ID for each checkbox
-const supported = ['disableresume', 'pauseoninactive', 'multipletabs'];
+const supported = ['disableresume', 'pauseoninactive', 'multipletabs', 'ignoretabchange'];
 
 var userinput = document.getElementById('userinput');
 

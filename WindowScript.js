3d595fa5783e5b36485de342ae610863b114cbb1
NDevTK <31563761+NDevTK@users.noreply.github.com>
2021-10-29 19:54:24 +0100

    Migration to page context

--- WindowScript.js
+++ WindowScript.js
@@ -5,21 +5,237 @@
   const play = window.HTMLMediaElement.prototype.play;
   let div = null;
   window.HTMLMediaElement.prototype.play = function() {
-    if (this instanceof HTMLMediaElement && !document.contains(this)) {
-      if (!document.contains(div)) {
-        div = document.createElement('div');
-        div.hidden = true;
-        document.head.appendChild(div);
-        // If media gets paused remove it from the div
-        div.addEventListener('pause', event => {
-          const src = event.srcElement;
-          if (src instanceof HTMLMediaElement) {
-            div.removeChild(src);
-          }
-        }, { passive: true, capture: true });
-      }
-      div.appendChild(this);
+    if (this instanceof HTMLMediaElement) {
+      addListener(this);
     }
     return play.apply(this, arguments);
   }
+  
+  // Script should only run once
+
+    if (hasProperty(window, "Elements")) return
+    
+    var Elements = new Set();
+	
+    chrome.runtime.onMessage.addListener(message => {
+        switch (message) {
+        case 'toggleFastPlayback':
+            toggleRate();
+            break
+        case 'Rewind':
+            Rewind();
+            break
+        case 'togglePlayback':
+            togglePlayback();
+            break
+        case 'allowplayback':
+            resume(false);
+            break
+        case 'next':
+            next();
+            break
+        case 'previous':
+            previous();
+            break
+        case 'pause':
+            pause();
+            break
+        case 'play':
+            // When there media already playing tell the background script.
+            if (isPlaying())
+                send('play');
+            resume(true);
+            break
+        }
+    });
+
+    function hasProperty(value, key) {
+        return Object.prototype.hasOwnProperty.call(value, key);
+    }
+
+    function togglePlayback() {
+        Elements.forEach(e => {
+            if (e.paused) return;
+            if (e.togglePause) {
+                e.togglePause = false;
+                e.playbackRate = e.wasPlaybackRate;
+            } else {
+                e.togglePause = true;
+                e.wasPlaying = false;
+                e.wasPlaybackRate = e.playbackRate;
+                e.playbackRate = 0;
+            }
+        });
+    }
+
+    function isPlaying() {
+        const audibleElements = [...Elements].filter(e => !e.muted);
+        return (audibleElements.length !== 0);
+    }
+
+    function isPaused(e) {
+        return (e.paused || e.playbackRate === 0);
+    }
+
+    function next() {
+        Elements.forEach(e => {
+            if (isPaused(e)) return;
+            e.currentTime = e.duration;
+        });
+    }
+
+    function previous() {
+        Elements.forEach(e => {
+            if (isPaused(e)) return;
+            // Unknown
+            e.currentTime = 0;
+        });
+    }
+
+    // Controlled by global fast forward shortcut
+    function toggleRate() {
+        Elements.forEach(e => {
+            if (isPaused(e))
+                return;
+            if (e.wasPlaybackRate && e.playbackRate > 1) {
+                e.playbackRate = e.wasPlaybackRate;
+            } else {
+                e.wasPlaybackRate = e.playbackRate;
+                e.playbackRate = 2;
+            }
+        });
+    }
+
+    // Controlled by global rewind shortcut
+    function Rewind() {
+        Elements.forEach(e => {
+            if (isPaused(e))
+                return;
+	    e.currentTime -= 30;
+        });
+    }
+
+    
+    function onPlay(e, trusted = false) {
+        if (e.muted) {
+            send('playMuted');
+        } else if (trusted) {   
+            send('playTrusted');
+            normalPlayback(e);
+        } else {
+            send('play');
+        }
+    }
+
+    function send(message) {
+	    document.dispatchEvent(new CustomEvent("autopause_result", {detail: message}));
+    }
+    
+    function addListener(target) {
+    Elements.add(target);
+    // On media play event
+    target.addEventListener('play', function (event) {
+        const src = event.srcElement;
+        if (src instanceof HTMLMediaElement) {
+            onPlay(src, event.isTrusted);
+            Elements.add(src);
+        }
+    }, {
+        capture: true,
+        passive: true
+    });
+
+    target.addEventListener('volumechange', function (event) {
+        const src = event.srcElement;
+        if (src instanceof HTMLMediaElement && !isPaused(src)) {
+            onPlay(src);
+        }
+    }, {
+        capture: true,
+        passive: true
+    });
+
+    target.addEventListener('pause', event => {
+        setTimeout(() => {
+            onPause(event);
+        }, 200);
+    }, {
+        capture: true,
+        passive: true
+    });
+
+    target.addEventListener('abort', event => {
+        onPause(event);
+    }, {
+        capture: true,
+        passive: true
+    });
+
+    // Dont tell the media please
+    target.addEventListener('ratechange', function (event) {
+        const src = event.srcElement;
+        if (src instanceof HTMLMediaElement) {
+            if (src.playbackRate === 0 && src.wasPlaying) {
+                event.stopPropagation();
+            }
+            if (!isPaused(src)) {
+                onPlay(src);
+            }
+        }
+    }, {
+        capture: true
+    });
+    }
+    function onPause(event) {
+        const src = event.srcElement;
+        if (src instanceof HTMLMediaElement && src.paused) {
+            // Check if all elements have paused.
+            Elements.delete(src);
+            normalPlayback(src);
+            if (!isPlaying()) {
+                send('pause');
+            }
+        }
+    }
+
+    function normalPlayback(src) {
+        if (src.wasPlaying) {
+            src.volume = src.wasVolume;
+            src.playbackRate = src.wasPlaybackRate;
+            src.wasPlaying = false;
+        }
+    }
+
+    function pauseElement(e) {
+        // If media attempts to play when it should be paused dont change its old values.
+        if (!e.wasPlaying) {
+            e.wasVolume = e.volume;
+            e.wasPlaybackRate = e.playbackRate;
+        }
+        // Rate change event will stopPropagation.
+        e.wasPlaying = true;
+        e.playbackRate = 0;
+        e.volume = 0;
+    }
+
+    async function pause() {
+        Elements.forEach(e => {
+            if (isPaused(e)) return;
+            pauseElement(e);
+        });
+    }
+
+    async function resume(shouldPlay) {
+        Elements.forEach(e => {
+            if (!document.contains(e)) {
+                Elements.delete(e);
+                return
+            }
+            if (!e.wasPlaying) return
+            // Pause foreground media normaly
+            if (shouldPlay === false)
+                e.pause();
+            normalPlayback(e);
+        });
+    }
 })();

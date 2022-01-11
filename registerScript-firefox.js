a6a3120da23c5fd0a7e9380dac0f374102dea0e9
NDevTK <31563761+NDevTK@users.noreply.github.com>
2022-01-11 09:34:04 +0000

    Update registerScript-firefox.js

--- registerScript-firefox.js
+++ registerScript-firefox.js
@@ -24,3 +24,7 @@ function registerScriptFirefox() {
 browser.permissions.onAdded.addListener(registerScriptFirefox);
 browser.permissions.onRemoved.addListener(registerScriptFirefox);
 registerScriptFirefox();
+
+function shadow(e) {
+  return e.openOrClosedShadowRoot();
+}

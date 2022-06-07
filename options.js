5a43e82f99f3fa35a66d67a9d397b91a4f205408
NDevTK <31563761+NDevTK@users.noreply.github.com>
2022-06-07 12:38:42 +0100

    Update options.js

--- options.js
+++ options.js
@@ -97,3 +97,52 @@ async function permissionUpdate() {
         });
     }
 }
+
+
+// datalist multiple support.
+document.addEventListener("DOMContentLoaded", function () {
+    const separator = ' ';
+
+    for (const input of document.getElementsByTagName("input")) {
+        if (!input.multiple) {
+            continue;
+        }
+
+        if (input.list instanceof HTMLDataListElement) {
+            const optionsValues = Array.from(input.list.options).map(opt => opt.value);
+            let valueCount = input.value.split(separator).length;
+
+            input.addEventListener("input", () => {
+                const currentValueCount = input.value.split(separator).length;
+
+                // Do not update list if the user doesn't add/remove a separator
+                // Current value: "a, b, c"; New value: "a, b, cd" => Do not change the list
+                // Current value: "a, b, c"; New value: "a, b, c," => Update the list
+                // Current value: "a, b, c"; New value: "a, b" => Update the list
+                if (valueCount !== currentValueCount) {
+                    const lsIndex = input.value.lastIndexOf(separator);
+                    const str = lsIndex !== -1 ? input.value.substr(0, lsIndex) + separator : "";
+                    filldatalist(input, optionsValues, str);
+                    valueCount = currentValueCount;
+                }
+            });
+        }
+    }
+
+    function filldatalist(input: HTMLInputElement, optionValues: string[], optionPrefix: string) {
+        const list = input.list;
+        if (list && optionValues.length > 0) {
+            list.innerHTML = "";
+
+            const usedOptions = optionPrefix.split(separator).map(value => value.trim());
+
+            for (const optionsValue of optionValues) {
+                if (usedOptions.indexOf(optionsValue) < 0) { // Skip used values
+                    const option = document.createElement("option");
+                    option.value = optionPrefix + optionsValue;
+                    list.append(option);
+                }
+            }
+        }
+    }
+});

d1209de35ecf04e32be2d997d5f6e06209733928
NDevTK <31563761+NDevTK@users.noreply.github.com>
2022-10-07 00:44:11 +0100

    Errorz

--- WindowScript.js
+++ WindowScript.js
@@ -5,20 +5,24 @@
   const play = window.HTMLMediaElement.prototype.play;
   let div = null;
   window.HTMLMediaElement.prototype.play = function() {
-    if (this instanceof HTMLMediaElement && !this.isConnected) {
-      if (!document.contains(div)) {
-        div = document.createElement('div');
-        div.hidden = true;
-        document.head.appendChild(div);
-        // If media gets paused remove it from the div
-        div.addEventListener('pause', event => {
-          const src = event.srcElement;
-          if (src instanceof HTMLMediaElement) {
-            div.removeChild(src);
-          }
-        }, { passive: true, capture: true });
+    try {
+      if (this instanceof HTMLMediaElement && !this.isConnected) {
+        if (!document.contains(div)) {
+          div = document.createElement('div');
+          div.hidden = true;
+          document.head.appendChild(div);
+          // If media gets paused remove it from the div
+          div.addEventListener('pause', event => {
+            const src = event.srcElement;
+            if (src instanceof HTMLMediaElement) {
+              div.removeChild(src);
+            }
+          }, { passive: true, capture: true });
+        }
+        div.appendChild(this);
       }
-      div.appendChild(this);
+    } catch {
+      // Extension errors should not affect the API.
     }
     return play.apply(this, arguments);
   }

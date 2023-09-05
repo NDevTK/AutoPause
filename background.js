c3d4f27b8ef6ef24d53d924605da0e260d2be8d2
NDevTK <31563761+NDevTK@users.noreply.github.com>
2023-09-05 18:07:47 +0100

    Update background.js

--- background.js
+++ background.js
@@ -245,11 +245,7 @@ chrome.commands.onCommand.addListener(async command => {
         Broadcast('Rewind');
         break
     case 'togglePlayback':
-        var result = getResumeTab();
-        if (result !== false) {
-            Broadcast('pause', result);
-            if (state.otherTabs.size === 0) send(result, 'togglePlayback');
-        }
+        Broadcast('togglePlayback');
         break
     case 'next':
         Broadcast('next');

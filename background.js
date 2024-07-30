d335f260b14fc3c869ecb41347f0f7b0c731903e
NDevTK <31563761+NDevTK@users.noreply.github.com>
2024-07-30 17:28:00 +0100

    Add deny pause (why not lol)

--- background.js
+++ background.js
@@ -422,6 +422,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 async function denyPause(id, exclude, skipLast, allowbg) {
     // Logic used to determine if the extension is allowed to pause automatically.
     if (state.denyPlayback) return false;
+    if (hasProperty(options, 'denypause')) return true;
     if (id === exclude) return true;
     if (allowbg && state.backgroundaudio.has(id)) return true;
     if (skipLast && id === state.lastPlaying) return true;
